6caf055b82e8d827c0611d294ff2b4e32b78497d
Bo Dincer <64631613+boVantage007@users.noreply.github.com>
2020-05-06 06:08:11 -0400

    page: https://freemantdsfdsf.pythonanywhere.com/
    updated.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """
 @author: BO DINCER
+@page: https://freemantdsfdsf.pythonanywhere.com/
 """
 
 from flask import Flask, render_template
@@ -8,14 +9,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    '''
+    RENDERS TO THE HOME PAGE TEMPLATE
+    '''
     return render_template('home.html')
 
 @app.route('/about/')
 def about():
+    '''
+    RENDERS TO THE ABOUT PAGE TEMPLATE
+    '''
     return render_template('about.html')
 
 @app.route('/assignments/')
 def assignments():
+    '''
+    RENDERS TO THE ASSIGNMENTS PAGE TEMPLATE
+    '''
     return render_template('assignments.html')
 
 if __name__ == '__main__':
